a668080ce1d6db3209d1a38128a78bafc520685a
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-17 16:40:40 +0200

    Update environment variables [frontend]
    Added backend server address variable

--- frontend/edgar-irt-webinterface/src/environments/environment.ts
+++ frontend/edgar-irt-webinterface/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  backendServerInfo: {
+    applicationAddress: "http://localhost:8970"
+  }
 };
 
 /*
