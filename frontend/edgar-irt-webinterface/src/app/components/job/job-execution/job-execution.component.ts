7a06bc929ed54b61ddc4ab37e880df2c1dc36ce0
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-20 01:54:22 +0200

    Finalized implementation of the job execution component [frontend]

--- frontend/edgar-irt-webinterface/src/app/components/job/job-execution/job-execution.component.ts
+++ frontend/edgar-irt-webinterface/src/app/components/job/job-execution/job-execution.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { IAcademicYear } from 'src/app/models/edgar/academic-year.model.js';
 import { IEdgarCourse } from 'src/app/models/edgar/course.model';
@@ -12,6 +12,9 @@ import { JobsService } from 'src/app/services/jobs.service';
   templateUrl: './job-execution.component.html',
 })
 export class JobExecutionComponent implements OnInit {
+  private static readonly MAX_PREVIOUS_INCLUDED_YEAR = 10;
+  readonly previousIncludedYearsChoices: number[] = [];
+
   selectedJobType: IJobType | null = null;
 
   jobExecutionForm: FormGroup = null!
@@ -25,19 +28,29 @@ export class JobExecutionComponent implements OnInit {
   constructor(
     private readonly jobsService: JobsService,
     private readonly edgarService: EdgarService,
-  ) { }
+  ) {
+    this.previousIncludedYearsChoices.splice(0, this.previousIncludedYearsChoices.length);
+    for (let i = 1; i <= JobExecutionComponent.MAX_PREVIOUS_INCLUDED_YEAR; ++i) {
+      this.previousIncludedYearsChoices.push(i);
+    }
+  }
 
   ngOnInit(): void {
     this.jobExecutionForm = new FormGroup(
       {
-        jobType: new FormControl(null, [Validators.required]),
+        idJobType: new FormControl(null, [Validators.required]),
 
-        course: new FormControl(null, [Validators.required]),
-        academicYear: new FormControl(null, [Validators.required]),
-        numberOfIncludedPreviousYears: new FormControl(null, [Validators.required]),
-        maxJobTimeoutMs: new FormControl(null, [Validators.required, Validators.min(0)]),
+        jobSpecificConfiguration: new FormGroup({
+          idCourse: new FormControl(null, [Validators.required]),
+          idStartcademicYear: new FormControl(null, [Validators.required]),
+          numberOfIncludedPreviousYears: new FormControl(null, [Validators.required]),
+          maxJobTimeoutMs: new FormControl(200000, [Validators.required, Validators.min(0)]),
+          forceCalculation: new FormControl(false, []),
+        }),
+
+        jobName: new FormControl(null, []),
+        userNote: new FormControl(null, []),
         periodical: new FormControl(false, []),
-        forceCalculation: new FormControl(false, []),
       }
     );
 
@@ -47,20 +60,53 @@ export class JobExecutionComponent implements OnInit {
   }
 
   advanceToFlowStep(flowStep: number) {
-    console.log(this.jobExecutionForm.getRawValue());
     this.jobExecutionFlowStep = flowStep;
 
+    const stepControls: (AbstractControl | null)[] = [];
+    stepControls.push(this.jobExecutionForm.get('jobType'));
+
     if (flowStep !== 0) {
-      this.jobExecutionForm.get('jobType')?.disable();
+      stepControls.forEach(sc => sc?.disable());
+    } else {
+      stepControls.forEach(sc => sc?.enable());
     }
 
+    stepControls.splice(0, stepControls.length);
+
+    stepControls.push(
+      this.jobExecutionForm.get('jobSpecificConfiguration'),
+      this.jobExecutionForm.get('jobName'),
+      this.jobExecutionForm.get('userNote'),
+      this.jobExecutionForm.get('periodical'),
+    );
+
     if (flowStep !== 1) {
-      this.jobExecutionForm.get('course')?.disable();
-      this.jobExecutionForm.get('academicYear')?.disable();
-      this.jobExecutionForm.get('numberOfIncludedPreviousYears')?.disable();
-      this.jobExecutionForm.get('maxJobTimeoutMs')?.disable();
-      this.jobExecutionForm.get('periodical')?.disable();
-      this.jobExecutionForm.get('forceCalculation')?.disable();
+      stepControls.forEach(sc => sc?.disable());
+    } else {
+      stepControls.forEach(sc => sc?.enable());
     }
+
+    stepControls.splice(0, stepControls.length);
+  }
+
+  startJob() {
+    const form = this.jobExecutionForm.getRawValue();
+
+    this.jobsService.startJob({
+      idJobType: parseInt(form.idJobType),
+      jobName: form.jobName,
+      userNote: form.userNote,
+      periodical: form.periodical,
+      request: form.jobSpecificConfiguration,
+    }).subscribe({
+        next: () => {
+          window.alert("Job start request sent successfully");
+          window.location.reload();
+        },
+        error: () => {
+          window.alert("Job was not able to be started");
+          window.location.reload();
+        }
+      });
   }
 }
