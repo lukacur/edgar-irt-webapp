df6ceb31c87e2276b47651c06e6181333c6245e4
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-23 01:15:38 +0200

    Added statistics service [frontend]
    This service is used for statistics-related backend queries

---
+++ frontend/edgar-irt-webinterface/src/app/services/statistics.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { IEdgarCourse } from '../models/edgar/course.model';
+import { environment } from 'src/environments/environment';
+import { ICourseCalculation } from '../models/statistics-processing/course-calculation.model.js';
+import { ICourseLevelStatisticsCalculation } from '../models/statistics-processing/course-level-statistics-calculation.model.js';
+import { ITestLevelStatisticsCalculation } from '../models/statistics-processing/test-level-statistics-calculation.model.js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatisticsService {
+
+  constructor(
+    private readonly http: HttpClient,
+  ) { }
+
+  getCoursesWithCalculatedStatistics(): Observable<IEdgarCourse[]> {
+    return this.http
+      .get<IEdgarCourse[]>(`${environment.backendServerInfo.applicationAddress}/statprocessing/courses`)
+      .pipe(tap(d => console.log(d)));
+  }
+
+  getCourseCalculations(idCourse: number): Observable<ICourseCalculation[]> {
+    return this.http
+      .get<ICourseCalculation[]>(
+        `${environment.backendServerInfo.applicationAddress}/statprocessing/${idCourse}/calculations`
+      )
+      .pipe(tap(d => console.log(d)));
+  }
+
+  getCourseLevelCalculations(calcGroup: string): Observable<ICourseLevelStatisticsCalculation[]> {
+    return this.http
+      .get<ICourseLevelStatisticsCalculation[]>(
+        `${environment.backendServerInfo.applicationAddress}/statprocessing/calculations/${calcGroup}/course-level`
+      )
+  }
+
+  getTestLevelCalculations(calcGroup: string): Observable<ITestLevelStatisticsCalculation[]> {
+    return this.http
+      .get<ITestLevelStatisticsCalculation[]>(
+        `${environment.backendServerInfo.applicationAddress}/statprocessing/calculations/${calcGroup}/test-level`
+      )
+  }
+}
