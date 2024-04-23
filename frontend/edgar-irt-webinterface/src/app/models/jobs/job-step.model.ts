89b7363513ab7c8a9f935d580f19b2e3e3099936
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-24 01:13:21 +0200

    Update job step model [frontend]
    Added job step name

--- frontend/edgar-irt-webinterface/src/app/models/jobs/job-step.model.ts
+++ frontend/edgar-irt-webinterface/src/app/models/jobs/job-step.model.ts
@@ -2,6 +2,7 @@ export interface IJobStep {
     id: string;
     started_on: string;
     finished_on: string;
+    name: string;
     job_step_status: "NOT_STARTED" | "RUNNING" | "SUCCESS" | "FAILURE" | "SKIP_CHAIN" | "CRITICALLY_ERRORED";
     job_step_status_message: string;
     ordinal: number;
