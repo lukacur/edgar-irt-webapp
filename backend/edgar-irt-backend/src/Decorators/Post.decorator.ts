ef5737d9c8577e37cf042bdbb9c540d2bb401131
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-24 20:52:18 +0200

    Added decorators used for registering controller endpoint handlers [backend]

---
+++ backend/edgar-irt-backend/src/Decorators/Post.decorator.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from "express";
+import { AbstractController } from "../Controlllers/AbstractController.js";
+import { ExpressServer } from "../ExpressServer.js";
+
+export function Post(relativeEndpoint: string) {
+    return <TTargetClass extends AbstractController, TDecoratedItem extends RequestHandler>(
+        target: TTargetClass,
+        propertyKey: string | symbol,
+        descriptor: TypedPropertyDescriptor<TDecoratedItem>
+    ) => {
+        const handler: RequestHandler | null = descriptor.value ?? null;
+        if (handler === null) {
+            throw new Error("Invalid handler");
+        }
+
+        const oldApplyController = target.applyController;
+
+        target.applyController = function (server: ExpressServer) {
+            this.registerEndpoint("POST", { relativeEndpoint, handlers: [handler.bind(this)] });
+            oldApplyController.bind(this)(server);
+        };
+    }
+}
