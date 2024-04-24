b415b033a1be336936cba0b8d4de5683eccce2ad
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-24 21:31:50 +0200

    Added Put and Delete decorators [backend]

---
+++ backend/edgar-irt-backend/src/Decorators/Put.decorator.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from "express";
+import { AbstractController } from "../Controlllers/AbstractController.js";
+import { ExpressServer } from "../ExpressServer.js";
+
+export function Put(relativeEndpoint: string) {
+    return <TTargetClass extends AbstractController, TDecoratedItem extends RequestHandler>(
+        target: TTargetClass,
+        propertyKey: string | symbol,
+        descriptor: TypedPropertyDescriptor<TDecoratedItem>
+    ) => {
+        const handler: RequestHandler | null = descriptor.value ?? null;
+        if (handler === null) {
+            throw new Error("Invalid handler");
+        }
+
+        const oldApplyController = target.applyController;
+
+        target.applyController = function (server: ExpressServer) {
+            this.registerEndpoint("PUT", { relativeEndpoint, handlers: [handler.bind(this)] });
+            oldApplyController.bind(this)(server);
+        };
+    }
+}
