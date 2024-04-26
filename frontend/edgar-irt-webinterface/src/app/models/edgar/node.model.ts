2cb5f2de5cd510b98edc3fc7ad6ce280caee33cc
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-26 21:07:32 +0200

    Update edgar service [frontend]
    Added getCourseNodes method for fetching all nodes related to a course

---
+++ frontend/edgar-irt-webinterface/src/app/models/edgar/node.model.ts
@@ -0,0 +1,9 @@
+export interface IEdgarNode {
+    id: number;
+
+    id_node_type: number;
+    node_type_name: string;
+
+    node_name: string;
+    description: string;
+}
--- frontend/edgar-irt-webinterface/src/app/services/edgar.service.ts
+++ frontend/edgar-irt-webinterface/src/app/services/edgar.service.ts
@@ -3,7 +3,8 @@ import { Observable, tap } from 'rxjs';
 import { IEdgarCourse } from '../models/edgar/course.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { IAcademicYear } from '../models/edgar/academic-year.model.js';
+import { IAcademicYear } from '../models/edgar/academic-year.model';
+import { IEdgarNode } from '../models/edgar/node.model';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,13 @@ export class EdgarService {
     return this.http
       .get<IAcademicYear[]>(
         `${environment.backendServerInfo.applicationAddress}/academic-years`
+      ).pipe(tap(data => console.log(data)));
+  }
+
+  getCourseNodes(idCourse: number): Observable<IEdgarNode[]> {
+    return this.http
+      .get<IEdgarNode[]>(
+        `${environment.backendServerInfo.applicationAddress}/course/${idCourse}/nodes`
       ).pipe(tap(data => console.log(data)));
   }
 }
