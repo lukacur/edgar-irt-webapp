e985f821313366f8590e860687dc0285e0c1160b
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-26 18:52:02 +0200

    Update searchable select component with multiselect functionallity [frontend]

--- frontend/edgar-irt-webinterface/src/app/components/common/searchable-select/searchable-select.component.ts
+++ frontend/edgar-irt-webinterface/src/app/components/common/searchable-select/searchable-select.component.ts
@@ -2,72 +2,110 @@ import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@
 import { AbstractControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-searchable-select',
-  templateUrl: './searchable-select.component.html',
+    selector: 'app-searchable-select',
+    templateUrl: './searchable-select.component.html',
 })
 export class SearchableSelectComponent implements OnInit {
-  @Input("selectableItems")
-  selectableItems: any[] = null!;
+    @Input("selectableItems")
+    selectableItems: any[] = null!;
 
-  @Input("valueKey")
-  valueKey: string = null!;
+    @Input("valueKey")
+    valueKey: string = null!;
 
-  @Input("textKey")
-  textKey: string = null!;
+    @Input("textKey")
+    textKey: string = null!;
 
-  @Input("labelText")
-  labelText: string = null!;
+    @Input("labelText")
+    labelText: string = null!;
 
-  @Input("ngClassObj")
-  ngClassObj: { [classes: string]: boolean } = null!;
+    @Input("ngClassObj")
+    ngClassObj: { [classes: string]: boolean } = null!;
 
-  @Input("control")
-  control: AbstractControl<any, any> = null!;
+    @Input("control")
+    control: AbstractControl<any, any> = null!;
 
-  @Input("searchable")
-  searchable: boolean = false;
+    @Input("searchable")
+    searchable: boolean = false;
 
-  @HostListener('window:keyup', ['$event'])
-  listenForEscape(event: KeyboardEvent) {
-    if (event.key.toLocaleLowerCase() === "escape") {
-      this.clearSearch();
+    @Input("multi")
+    multi: boolean = false;
+
+    @HostListener('window:keyup', ['$event'])
+    listenForEscape(event: KeyboardEvent) {
+        if (event.key.toLocaleLowerCase() === "escape") {
+            this.clearSearch();
+        }
     }
-  }
 
-  @HostListener('window:click', ['$event.target'])
-  listenForOutClick(clickTarget: HTMLElement) {
-    if ((this.choiceList ?? null) !== null && !this.choiceList?.nativeElement.contains(clickTarget)) {
-      this.optionsExpanded = false;
+    @HostListener('window:click', ['$event.target'])
+    listenForOutClick(clickTarget: HTMLElement) {
+        if ((this.choiceList ?? null) !== null && !this.choiceList?.nativeElement.contains(clickTarget)) {
+            this.optionsExpanded = false;
+        }
     }
-  }
 
 
-  @ViewChild("searchInput")
-  searchInput?: ElementRef<HTMLInputElement> | null = null;
+    @ViewChild("searchInput")
+    searchInput?: ElementRef<HTMLInputElement> | null = null;
 
-  @ViewChild("choiceList")
-  choiceList?: ElementRef<HTMLDivElement> | null = null;
+    @ViewChild("choiceList")
+    choiceList?: ElementRef<HTMLDivElement> | null = null;
 
 
-  searchTerm: string | null = null;
+    searchTerm: string | null = null;
 
-  selectValue: any = null;
-  selectText: any = null;
+    selectedItems: any[] = [];
 
-  optionsExpanded: boolean = false;
+    selectText: any = null;
 
-  constructor() { }
+    optionsExpanded: boolean = false;
 
-  ngOnInit(): void {}
+    constructor() { }
 
-  getFilteredItems() {
-    return this.selectableItems.filter(
-      it => it[this.textKey].toLocaleLowerCase().includes(this.searchTerm?.toLocaleLowerCase() ?? "")
-    );
-  }
+    ngOnInit(): void {
+        if (this.multi) {
+            this.control.setValue(this.selectedItems);
+        }
+    }
 
-  clearSearch() {
-    this.searchTerm = "";
-    this.searchInput?.nativeElement.focus();
-  }
+    select(value: any): void {
+        if (this.selectedItems.includes(value)) {
+            return;
+        }
+
+        if (!this.multi) {
+            this.selectedItems.splice(0, this.selectedItems.length);
+        }
+
+        this.selectedItems.push(value);
+    }
+
+    remove(value: any): void {
+        const idx = this.selectedItems.indexOf(value);
+
+        if (idx === -1) {
+            return;
+        }
+
+        this.selectedItems.splice(idx, 1);
+    }
+
+    getSelectedItemValues() {
+        return (this.multi) ?
+            (this.selectedItems.map(it => it[this.valueKey])) :
+            ((this.selectedItems.length <= 0) ? null : this.selectedItems[0][this.valueKey]);
+    }
+
+    getFilteredItems(): any[] {
+        return this.selectableItems
+            .filter(
+                it => it[this.textKey].toLocaleLowerCase().includes(this.searchTerm?.toLocaleLowerCase() ?? "")
+            )
+            .filter(it => !this.multi || !this.selectedItems.includes(it));
+    }
+
+    clearSearch(): void {
+        this.searchTerm = "";
+        this.searchInput?.nativeElement.focus();
+    }
 }
