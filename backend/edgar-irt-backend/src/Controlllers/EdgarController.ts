1af88ff519cfad6051f730a64716c630c9f2b2c5
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-26 21:07:58 +0200

    Update EdgarController with course node fetch method [backend]

--- backend/edgar-irt-backend/src/Controlllers/EdgarController.ts
+++ backend/edgar-irt-backend/src/Controlllers/EdgarController.ts
@@ -4,6 +4,7 @@ import { Get } from "../Decorators/Get.decorator.js";
 import { AbstractController } from "./AbstractController.js";
 import { IEdgarAcademicYear } from "../Models/Database/Edgar/IEdgarAcademicYear.js";
 import { IEdgarCourse } from "../Models/Database/Edgar/IEdgarCourse.js";
+import { IEdgarNode } from "../Models/Database/Edgar/IEdgarNode.js";
 
 export class EdgarController extends AbstractController {
     constructor(
@@ -35,5 +36,51 @@ export class EdgarController extends AbstractController {
         res
             .status(200)
             .json(academicYears);
+    }
+
+    @Get("course/:idCourse/nodes")
+    public async getCourseDeclaredNodes(req: Request, res: Response, next: NextFunction): Promise<void> {
+        const idCourse = req.params['idCourse'];
+        if ((idCourse ?? null) === null) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const courseNodes: IEdgarNode[] = (
+            await this.dbConn.doQuery<IEdgarNode>(
+                `WITH RECURSIVE course_sub_nodes (id, id_node_type, node_name, description)
+                AS(
+                    SELECT node.id,
+                            node.id_node_type,
+                            node.node_name,
+                            node.description
+                    FROM course
+                        JOIN node
+                            ON course.id_root_node = node.id
+                    WHERE course.id = $1
+                
+                    UNION
+
+                    SELECT node.id,
+                            node.id_node_type,
+                            node.node_name,
+                            node.description
+                    FROM course_sub_nodes
+                        JOIN node_parent
+                            ON course_sub_nodes.id = node_parent.id_parent
+                        JOIN node
+                            ON node_parent.id_child = node.id
+                ) 
+                SELECT course_sub_nodes.*,
+                        node_type.type_name AS node_type_name
+                FROM course_sub_nodes
+                    JOIN node_type
+                        ON course_sub_nodes.id_node_type = node_type.id
+                ORDER BY course_sub_nodes.node_name`,
+                [idCourse]
+            )
+        )?.rows ?? [];
+
+        res.send(courseNodes);
     }
 }
