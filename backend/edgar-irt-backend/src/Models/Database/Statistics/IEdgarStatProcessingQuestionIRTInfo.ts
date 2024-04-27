0823863c6ff92cb89cccb2610d9b69ec13bb66ea
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-28 00:48:26 +0200

    Update the IEdgarStatProcessingQuestionIRTInfo model with comments [backend]

--- backend/edgar-irt-backend/src/Models/Database/Statistics/IEdgarStatProcessingQuestionIRTInfo.ts
+++ backend/edgar-irt-backend/src/Models/Database/Statistics/IEdgarStatProcessingQuestionIRTInfo.ts
@@ -1,7 +1,17 @@
 export interface IEdgarStatProcessingQuestionIRTInfo {
     id_course_based_info: number;
+
+    /**
+     * Not included when using default SELECT * query (have to do JOIN)
+     */
     id_course: number;
+    /**
+     * Not included when using default SELECT * query (have to do JOIN)
+     */
     calculation_group: string;
+    /**
+     * Not included when using default SELECT * query (have to do JOIN)
+     */
     id_academic_years: number[];
 
     id_test_based_info: number[];
