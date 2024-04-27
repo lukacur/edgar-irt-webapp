f4600c382ff7d26494e9412e568a0c6c937ed9e4
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-28 00:04:15 +0200

    Update adaptive exercise progression service [frontend]
    Added methods for activating and handling currently active student exercise

--- frontend/edgar-irt-webinterface/src/app/services/adaptive-exercise-progression.service.ts
+++ frontend/edgar-irt-webinterface/src/app/services/adaptive-exercise-progression.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.js';
-import { IExerciseInstance } from '../models/adaptive-exercises/exercise-instance.model.js';
-import { HttpClient } from '@angular/common/http/index.js';
-import { IExerciseInstanceQuestion } from '../models/adaptive-exercises/exercise-instance-question.model.js';
+import { BehaviorSubject, firstValueFrom, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IExerciseInstance } from '../models/adaptive-exercises/exercise-instance.model';
+import { HttpClient } from '@angular/common/http';
+import { IExerciseInstanceQuestion } from '../models/adaptive-exercises/exercise-instance-question.model';
+import { ICurrentExercise } from '../models/adaptive-exercises/current-exercise.model';
+
+type StartExerciseRequest = {
+    idStudent: number | null;
+    idCourse: number;
+    questionsCount: number;
+    considerPreviousExercises: boolean;
+};
 
 @Injectable({
     providedIn: 'root'
@@ -11,17 +19,21 @@ import { IExerciseInstanceQuestion } from '../models/adaptive-exercises/exercise
 export class AdaptiveExerciseProgressionService {
     private readonly idTestStudent: number | null = environment.production ? null : 23; // id_app_user is 46
 
+    private readonly activatedExerciseInstanceInfo: BehaviorSubject<ICurrentExercise | null> =
+        new BehaviorSubject<ICurrentExercise | null>(null);
+
     constructor(
         private readonly http: HttpClient,
     ) { }
 
-    public getStudentExercises(idStudent: number | null): Observable<IExerciseInstance[]> {
+    public getStudentPreviousExercises(idStudent: number | null, idCourse: number): Observable<IExerciseInstance[]> {
         idStudent ??= this.idTestStudent;
 
         if (!environment.production) {
             return this.http
-                .get<IExerciseInstance[]>(
-                    `${environment.backendServerInfo.applicationAddress}/adaptive-exercises/${idStudent}`
+                .post<IExerciseInstance[]>(
+                    `${environment.backendServerInfo.applicationAddress}/adaptive-exercises/previous`,
+                    { idStudent, idCourse }
                 );
         }
 
@@ -29,10 +41,40 @@ export class AdaptiveExerciseProgressionService {
             .get<IExerciseInstance[]>(`${environment.backendServerInfo.applicationAddress}/adaptive-exercises`);
     }
 
-    public getStudentCurrentExercise(idStudent: number | null): Observable<IExerciseInstance | null> {
+    public getStudentCurrentExercise(idStudent: number | null, idCourse: number): Observable<ICurrentExercise | null> {
         return this.http
-            .get<IExerciseInstance | null>(
-                `${environment.backendServerInfo.applicationAddress}/adaptive-exercises/${idStudent}/current`
+            .post<ICurrentExercise | null>(
+                `${environment.backendServerInfo.applicationAddress}/adaptive-exercises/current`,
+                { idStudent: idStudent ?? ((environment.production) ? null : this.idTestStudent), idCourse }
+            );
+    }
+
+    public activateCurrentExercise(idStudent: number | null, idCourse: number): Observable<ICurrentExercise> {
+        return new Observable((subject) => {
+            (async () => {
+                const currentExercise = await firstValueFrom(this.getStudentCurrentExercise(idStudent, idCourse));
+                if (currentExercise === null) {
+                    subject.error(new Error("Student does not have a currently active exercise"));
+                }
+
+                this.activatedExerciseInstanceInfo.next(currentExercise);
+                subject.next(this.activatedExerciseInstanceInfo.value!);
+            })();
+        });
+    }
+
+    public getActivatedExercise(): Observable<ICurrentExercise | null> {
+        return this.activatedExerciseInstanceInfo.asObservable();
+    }
+
+    public startExercise(startExerciseRequest: StartExerciseRequest): Observable<ICurrentExercise> {
+        const finalizedRequest = { ...startExerciseRequest };
+        finalizedRequest.idStudent ??= this.idTestStudent;
+
+        return this.http
+            .post<ICurrentExercise>(
+                `${environment.backendServerInfo.applicationAddress}/adaptive-exercises/start-exercise`,
+                finalizedRequest,
             );
     }
 
