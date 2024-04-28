1db329221b29e8ad96686128da540aa2c0eaf067
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-28 23:39:55 +0200

    Update course and test level models to match new calculation principle [frontend]

--- frontend/edgar-irt-webinterface/src/app/models/statistics-processing/course-level-statistics-calculation.model.ts
+++ frontend/edgar-irt-webinterface/src/app/models/statistics-processing/course-level-statistics-calculation.model.ts
@@ -1,14 +1,14 @@
 export interface ICourseLevelStatisticsCalculation {
     id_question: number;
 
-    score_mean: number;
-    score_std_dev: number;
-    score_median: number;
+    score_perc_mean: number;
+    score_perc_std_dev: number;
+    score_perc_median: number;
     total_achieved: number;
     total_achievable: number;
     answers_count: number;
-    correct: number;
-    incorrect: number;
-    unanswered: number;
-    partial: number;
+    correct_perc: number;
+    incorrect_perc: number;
+    unanswered_perc: number;
+    partial_perc: number;
 }
