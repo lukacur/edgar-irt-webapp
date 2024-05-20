400b21cbf700a77e2a2e3e30a933939d4ba10a37
lukacur <92858303+lukacur@users.noreply.github.com>
2024-05-20 23:59:34 +0200

    Update QuestionClassificationUtil [backend]
    Added classDifference method

--- backend/edgar-irt-backend/src/Util/QuestionClassificationUtil.ts
+++ backend/edgar-irt-backend/src/Util/QuestionClassificationUtil.ts
@@ -26,4 +26,18 @@ export class QuestionClassificationUtil {
 
         return index1 - index2;
     }
+
+    public getClassDifference(class1: QuestionIrtClassification, class2: QuestionIrtClassification): number {
+        const index1 = this.classificationOrder.indexOf(class1);
+        if (index1 === -1) {
+            throw new Error(`${class1} is not a valid question classification`);
+        }
+
+        const index2 = this.classificationOrder.indexOf(class2);
+        if (index2 === -1) {
+            throw new Error(`${class2} is not a valid question classification`);
+        }
+
+        return Math.abs(index2 - index1);
+    }
 }
