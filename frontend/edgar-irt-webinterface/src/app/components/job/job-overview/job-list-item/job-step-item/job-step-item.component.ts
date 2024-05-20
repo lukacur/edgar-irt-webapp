6ac1d6c3c56c780efc7ead394ea09620947619fd
lukacur <92858303+lukacur@users.noreply.github.com>
2024-05-21 00:27:01 +0200

    Update JobUtil [frontend]
    Added job step status text and status color fetch

--- frontend/edgar-irt-webinterface/src/app/components/job/job-overview/job-list-item/job-step-item/job-step-item.component.ts
+++ frontend/edgar-irt-webinterface/src/app/components/job/job-overview/job-list-item/job-step-item/job-step-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IJobStep } from 'src/app/models/jobs/job-step.model.js';
+import { JobUtil } from 'src/app/util/job.util';
 
 @Component({
   selector: 'app-job-step-item',
@@ -12,27 +13,11 @@ export class JobStepItemComponent implements OnInit {
   constructor() { }
 
   getJobStepStatusText() {
-    switch (this.jobStep.job_step_status) {
-      case 'NOT_STARTED': return "Not started";
-      case 'RUNNING': return "Running...";
-      case 'SUCCESS': return "Success";
-      case 'FAILURE': return "Failure";
-      case 'SKIP_CHAIN': return "Skip chain";
-      case 'CRITICALLY_ERRORED': return "Critically errored";
-      default: return this.jobStep.job_step_status;
-    }
+    return JobUtil.getJobStepStatusText(this.jobStep);
   }
 
   getJobStepStatusColor() {
-    switch (this.jobStep.job_step_status) {
-      case 'RUNNING': return 'text-yellow-500';
-      case 'SUCCESS': return 'text-green-500';
-      case 'FAILURE': return 'text-red-500';
-      case 'NOT_STARTED': return 'text-orange-500';
-      case 'SKIP_CHAIN': return 'text-gray-500';
-      case 'CRITICALLY_ERRORED': return 'text-red-800';
-      default: return 'text-slate-500';
-    }
+    return JobUtil.getJobStepStatusColor(this.jobStep);
   }
 
   ngOnInit(): void {
--- frontend/edgar-irt-webinterface/src/app/util/job.util.ts
+++ frontend/edgar-irt-webinterface/src/app/util/job.util.ts
@@ -1,3 +1,4 @@
+import { IJobStep } from "../models/jobs/job-step.model.js";
 import { IJob } from "../models/jobs/job.model.js";
 
 export class JobUtil {
@@ -17,6 +18,30 @@ export class JobUtil {
             case 'RUNNING': return 'text-yellow-500';
             case 'FINISHED': return 'text-green-500';
             case 'FAILED': return 'text-red-500';
+            default: return 'text-slate-500';
+        }
+    }
+
+    public static getJobStepStatusText(jobStep: IJobStep) {
+        switch (jobStep.job_step_status) {
+            case 'NOT_STARTED': return "Not started";
+            case 'RUNNING': return "Running...";
+            case 'SUCCESS': return "Success";
+            case 'FAILURE': return "Failure";
+            case 'SKIP_CHAIN': return "Skip chain";
+            case 'CRITICALLY_ERRORED': return "Critically errored";
+            default: return jobStep.job_step_status;
+        }
+    }
+
+    public static getJobStepStatusColor(jobStep: IJobStep) {
+        switch (jobStep.job_step_status) {
+            case 'RUNNING': return 'text-yellow-500';
+            case 'SUCCESS': return 'text-green-500';
+            case 'FAILURE': return 'text-red-500';
+            case 'NOT_STARTED': return 'text-orange-500';
+            case 'SKIP_CHAIN': return 'text-gray-500';
+            case 'CRITICALLY_ERRORED': return 'text-red-800';
             default: return 'text-slate-500';
         }
     }
