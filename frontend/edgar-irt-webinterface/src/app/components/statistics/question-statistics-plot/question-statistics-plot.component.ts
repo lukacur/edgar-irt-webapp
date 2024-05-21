efd8c488d58831e1d681a29a0639973f174789b8
lukacur <92858303+lukacur@users.noreply.github.com>
2024-05-22 01:32:50 +0200

    Update QuestionStatisticsPlotComponent [frontend]
    Added interaction logic and display of all course-level question information for the given graph selections

--- frontend/edgar-irt-webinterface/src/app/components/statistics/question-statistics-plot/question-statistics-plot.component.ts
+++ frontend/edgar-irt-webinterface/src/app/components/statistics/question-statistics-plot/question-statistics-plot.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { map, Observable, Subscription, take } from 'rxjs';
+import { BehaviorSubject, map, Observable, Subscription, take } from 'rxjs';
 import { IEdgarCourse } from 'src/app/models/edgar/course.model.js';
 import { ICourseLevelStatisticsCalculation } from 'src/app/models/statistics-processing/course-level-statistics-calculation.model.js';
 import { StatisticsService } from 'src/app/services/statistics.service';
@@ -10,11 +10,15 @@ import { QuestionUtil } from 'src/app/util/question.util';
     selector: 'app-question-info-plot',
     templateUrl: './question-statistics-plot.component.html',
 })
-export class QuestionStatisticsPlotComponent implements OnInit {
+export class QuestionStatisticsPlotComponent implements OnInit, AfterViewInit {
+    readonly QUtil = QuestionUtil;
+
     readonly questionStatisticsPlotForm: FormGroup = new FormGroup({
         course: new FormControl<IEdgarCourse | null>(null),
         idCalculation: new FormControl<string | null>(null),
     });
+
+    readonly clearSelection = new BehaviorSubject<"ALL" | string>("");
 
     courses$: Observable<{ text: string, course: IEdgarCourse }[]> | null = null;
     courseCalculations: { calculationGroup: string, acYears: string }[] = [];
@@ -22,6 +26,8 @@ export class QuestionStatisticsPlotComponent implements OnInit {
     selectedCalculation: { calculationGroup: string, acYears: string } | null = null;
 
     courseLevelCalcs: ICourseLevelStatisticsCalculation[] | null = null;
+
+    readonly focusedCourseLevelCalculations: ICourseLevelStatisticsCalculation[] = [];
 
     private readonly subscriptions: Subscription[] = [];
 
@@ -45,6 +51,60 @@ export class QuestionStatisticsPlotComponent implements OnInit {
         this.subscriptions.push(
             this.questionStatisticsPlotForm.get("course")!.valueChanges.subscribe(_ => this.courseSelected())
         )
+    }
+
+    @ViewChild('compBase')
+    compBase: ElementRef<HTMLDivElement> | null = null;
+
+    chartsBaseWidth: number | null = null;
+
+    ngAfterViewInit(): void {
+        if ((this.compBase?.nativeElement ?? null) !== null) {
+            this.chartsBaseWidth = Math.max(window.innerWidth - 200, this.compBase!.nativeElement.clientWidth);
+        }
+    }
+
+    isMediumDeviceWidth() {
+        return window.innerWidth >= 768;
+    }
+
+    showHelp(event: MouseEvent, helpSpan: HTMLSpanElement) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (helpSpan.classList.contains('hidden')) {
+            helpSpan.classList.replace('hidden', 'inline');
+        } else {
+            helpSpan.classList.replace('inline', 'hidden');
+        }
+    }
+
+    focusCalculations(
+        calcs: ICourseLevelStatisticsCalculation[],
+        from: "bar-chart" | "histogram",
+        ...clearChannels: ("ALL" | string)[]
+    ): void {
+        let theCalculations: ICourseLevelStatisticsCalculation[];
+        switch (from) {
+            case 'histogram': {
+                theCalculations = calcs.map(clc => ({
+                    ...clc,
+                    score_perc_mean: clc.score_perc_mean / 100,
+                    score_perc_std_dev: clc.score_perc_std_dev / 100,
+                    score_perc_median: clc.score_perc_median / 100,
+                }));
+                break;
+            }
+
+            case 'bar-chart':
+            default: theCalculations = calcs;
+        }
+        this.focusedCourseLevelCalculations.splice(0, this.focusedCourseLevelCalculations.length);
+        this.focusedCourseLevelCalculations.push(...theCalculations);
+
+        if (clearChannels.length !== 0) {
+            clearChannels.forEach(ch => this.clearSelection.next(ch));
+        }
     }
 
     private getCalculations$() {
@@ -83,20 +143,30 @@ export class QuestionStatisticsPlotComponent implements OnInit {
     }
 
     courseSelected() {
+        this.selectedCalculation = null;
+        this.questionStatisticsPlotForm.get('idCalculation')?.setValue(null);
+
         if ((this.questionStatisticsPlotForm.get("course")?.value ?? null) === null) {
             return;
         }
-
-        this.selectedCalculation = null;
-        this.questionStatisticsPlotForm.get('idCalculation')?.setValue(null);
 
         this.getCalculations$()
             .pipe(take(1))
             .subscribe(calcs => this.courseCalculations = calcs);
     }
 
-    selectCalculation(calculation: { calculationGroup: string, acYears: string }) {
+    formatCalculationOptionTextCallback(calculation: { calculationGroup: string, acYears: string }): string {
+        return `(${calculation.calculationGroup }) Ac. years ${ calculation.acYears }`;
+    }
+
+    selectCalculation(calculation: { calculationGroup: string, acYears: string } | null) {
         this.selectedCalculation = calculation;
+        this.preparedBarChartData = null;
+        this.preparedHistogramData = null;
+
+        if (this.selectedCalculation === null) {
+            return;
+        }
 
         this.statisticsService.getCourseLevelCalculations(this.selectedCalculation.calculationGroup)
             .pipe(take(1))
@@ -107,35 +177,48 @@ export class QuestionStatisticsPlotComponent implements OnInit {
     readonly questionClassesColors = QuestionUtil.questionClassHexColors();
 
     //#region Difficulty bar chart init
-    prepareDifficultyBarChartData(): { qClass: string, count: number }[] {
-        const classificationObj: { [key: string]: number } = {};
-        for (const questionClass of this.availableQuestionClasses) {
-            classificationObj[questionClass] = 0;
+    private preparedBarChartData: { qClass: string, calculations: ICourseLevelStatisticsCalculation[] }[] | null = null;
+
+    prepareDifficultyBarChartData(): { qClass: string, calculations: ICourseLevelStatisticsCalculation[] }[] {
+        if (this.preparedBarChartData !== null) {
+            return this.preparedBarChartData;
         }
 
-        this.courseLevelCalcs?.forEach(clc => classificationObj[clc.question_irt_classification]++);
-
-        const retArr: { qClass: string, count: number }[] = [];
+        const retArr: { qClass: string, calculations: ICourseLevelStatisticsCalculation[] }[] = [];
 
         for (const questionClass of this.availableQuestionClasses) {
-            retArr.push({ qClass: questionClass, count: classificationObj[questionClass] });
+            retArr.push({
+                qClass: questionClass,
+                calculations:
+                    this.courseLevelCalcs?.filter(el => el.question_irt_classification === questionClass) ?? []
+            });
         }
 
-        return retArr;
+        return this.preparedBarChartData = retArr;
     }
     //#endregion
 
+    
     //#region Histogram data
-    prepareMeanHistogramData(): number[] {
-        return this.courseLevelCalcs?.map(clc => clc.score_perc_mean * 100) ?? [];
-    }
+    private preparedHistogramData: ICourseLevelStatisticsCalculation[] | null = null;
 
-    prepareStdDevHistogramData() {
-        return this.courseLevelCalcs?.map(clc => clc.score_perc_std_dev * 100) ?? [];
-    }
+    prepareHistogramData() {
+        if (this.preparedHistogramData !== null) {
+            return this.preparedHistogramData;
+        }
 
-    prepareMedianHistogramData() {
-        return this.courseLevelCalcs?.map(clc => clc.score_perc_median * 100) ?? [];
+        return this.preparedHistogramData =
+        (
+            this.courseLevelCalcs
+                ?.map(
+                    clc => ({
+                        ...clc,
+                        score_perc_mean: clc.score_perc_mean * 100,
+                        score_perc_std_dev: clc.score_perc_std_dev * 100,
+                        score_perc_median: clc.score_perc_median * 100,
+                    })
+                )
+        ) ?? [];
     }
     //#endregion
 
