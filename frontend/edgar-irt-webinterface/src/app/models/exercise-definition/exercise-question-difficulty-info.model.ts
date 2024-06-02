325d5f94a59d649e3336ae73f2e532c8633696af
lukacur <92858303+lukacur@users.noreply.github.com>
2024-06-02 15:22:15 +0200

    Update exercise question difficulty info model to match new logic [frontend]

--- frontend/edgar-irt-webinterface/src/app/models/exercise-definition/exercise-question-difficulty-info.model.ts
+++ frontend/edgar-irt-webinterface/src/app/models/exercise-definition/exercise-question-difficulty-info.model.ts
@@ -3,7 +3,7 @@ import { QuestionIrtClassification } from "src/app/util/question.util.js";
 export interface IQuestionDifficultyInfo {
     id_question: number;
     question_text: string;
-    id_node: number;
     question_difficulty: QuestionIrtClassification | null;
+    question_difficulty_override: QuestionIrtClassification | null;
     is_override: boolean;
 }
